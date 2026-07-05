b9a1930301108729643254bf581d64216ca723de
m77iladkamali <m77iladkamali@gmail.com>
2026-07-05 14:27:37 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.sinapsycho.consult2"
+        applicationId = "com.sinapsycho.consult3"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
